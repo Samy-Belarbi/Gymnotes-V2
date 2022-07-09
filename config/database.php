31a744d40756358850c82829a35f698c457b98a4
Samy <samy.belarbi@3wa.io>
2022-07-09 19:10:17 +0200

    new sql db

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
-    'dbname' => 'Gymnotes',
-    'user' => 'root',
-    'password' => ''
+    'host' => 'sql11.freemysqlhosting.net',
+    'dbname' => 'sql11505084',
+    'user' => 'sql11505084',
+    'password' => 'xuRxAeyK6B'
 ];

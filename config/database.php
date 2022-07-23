bc3f4e2a8c0518455fc3e52c11d16d5ceb443566
Samy <samy.belarbi@3wa.io>
2022-07-23 11:02:24 +0200

    corrections

--- config/database.php
+++ config/database.php
@@ -1,8 +1,15 @@
 <?php
 
+// return [
+//     'host' => 'sql11.freemysqlhosting.net',
+//     'dbname' => 'sql11505084',
+//     'user' => 'sql11505084',
+//     'password' => 'xuRxAeyK6B'
+// ];
+
 return [
-    'host' => 'sql11.freemysqlhosting.net',
-    'dbname' => 'sql11505084',
-    'user' => 'sql11505084',
-    'password' => 'xuRxAeyK6B'
+    'host' => '127.0.0.1',
+    'dbname' => 'Gymnotes',
+    'user' => 'root',
+    'password' => ''
 ];
